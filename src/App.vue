a6d105746ca1c131657f9696c9181c58da67a6ef
gd.pang <gd.pang@foxmail.com>
2020-07-10 15:53:53 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+      <div class="test"></div>
+      <p>你好呀</p>
+  </div>
+</template>
+
+<style lang="scss">
+    .test {
+        width: 300px;
+        height: 300px;
+        background-color: red;
+    }
+</style>
